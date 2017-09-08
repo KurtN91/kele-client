7f01fff9c172862c4887a7a70f34343b8760423a
kevrok <kurt.neaderhiser@gmail.com>
2017-09-08 18:52:28 +0000

    checkpoint 4 done

--- kele.gemspec
+++ kele.gemspec
@@ -6,11 +6,10 @@
    s.description   = 'A client for the Bloc API'
    s.authors       = ['Kurt Neaderhiser']
    s.email         = 'kurt.neaderhiser@gmail.com'
-   s.files         = ['lib/kele.rb', 'lib/kele.rb']
+   s.files         = ['lib/kele.rb']
    s.require_paths = ["lib"]
    s.homepage      =
      'http://rubygems.org/gems/kele'
    s.license       = 'MIT'
    s.add_runtime_dependency 'httparty', '~> 0.13'
-   s.add_runtime_dependency 'json', '~> 1.8', '>= 1.8.3'
  end
--- lib/kele.rb
+++ lib/kele.rb
@@ -1,19 +1,35 @@
+require "httparty"
+
 class Kele 
+    include HTTParty
 
-@authToken    
+    def new (email, password)
+        @email = email
+        @password = password
+        initialize(email, password)
+    end
+    
+    def initialize (email, password)
+        @base_uri =  'https://www.bloc.io/api/v1'
+        response = Kele.post("#{@base_uri}/sessions", body: {email: @email, password: @password})
+        @id = response["id"]
+        puts response
+        if response && response["auth_token"] 
+            @auth_token = response["auth_token"]
+            puts "#{email} was logged in"
+        else
+            puts "login failed yo"
+        end
+    end
+    
+    
+    def get_me
+        response = self.class.get(@base_uri, headers: { "authorization" => @auth_token })
+        return JSON.parse(response)
+    end
 
-def new (baseUri, email, password)
-    @baseUri =  baseUri
-    @email = email
-    @password = password
-
-    @authToken = self.class.post(baseUri, body: {login_email_address: email, login_password: password})
-end
-
-
-def get_me
-    response = self.class.get(baseUri, headers: { "authorization" => @authToken })
-    return JSON.parse(response)
-end
-
+    def get_mentor
+        response = self.class.get(@base_uri, headers: { "authorization" => @auth_token }, body: {id: @id})
+        return JSON.parse(response)
+    end
 end
